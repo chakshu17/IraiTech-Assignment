c4b68111e94b1a420d3a22334827ffdeaa0f71c0
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-12-20 13:47:48 +0530

    Update

--- src/app/components/question-three/question-three.component.ts
+++ src/app/components/question-three/question-three.component.ts
@@ -1,40 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-question-three',
   templateUrl: './question-three.component.html',
-  styleUrls: ['./question-three.component.scss']
+  styleUrls: ['./question-three.component.scss'],
 })
 export class QuestionThreeComponent implements OnInit {
-
-  constructor() { }
   userForm: FormGroup;
-  number: number[] = [];
-  ngOnInit(): void {
-    this.userForm = new FormGroup({
-      phoneNumber: new FormControl(''),
-      extraNumbers: new FormArray([]),
-    });
+
+  constructor(private fb: FormBuilder) {
+    // this.userForm = this.fb.group({
+    //   phoneNumbers: this.fb.array([]),
+    // });
+  }
+
+  phoneNumbers = new FormArray([]);
+
+  ngOnInit(): void {;
+  }
+
+
+  addNumber() {
+    this.phoneNumbers.push(new FormControl(''));
   }
 
   submit() {
-    this.number.push(this.userForm.value.extraNumbers);
-    this.number.push(this.userForm.value.phoneNumber);
-    console.log(this.number);
+    console.log(Object.assign({}, this.phoneNumbers.value));
   }
 
-  addNumber() {
-    (<FormArray>this.userForm.get('extraNumbers')).push(new FormControl(''));
-  }
-
-  addData(event){
-    console.log(event.target.value);
-
-    // (<FormArray>this.userForm.get('extraNumbers'))
-  }
   delete(index) {
-    (<FormArray>this.userForm.get('extraNumbers')).removeAt(index);
+    // (<FormArray>this.userForm.get('phoneNumbers')).removeAt(index);
+    this.phoneNumbers.removeAt(index);
   }
-
 }
